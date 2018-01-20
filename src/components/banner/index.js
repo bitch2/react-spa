f60b58b576faf05f7ced8317a4d447a1615261f1
rk <471091127@qq.com>
2018-01-20 21:14:24 +0800

    banner

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -1,24 +1,12 @@
 import React, {Component} from 'react'
-import {Transition} from 'react-transition-group'
 import FA from 'react-fontawesome'
 import './index.scss'
 import banner from '../../assets/img/banner.jpg'
-const defaultStyle = {
-    transition: 'opacity 500ms ease-in-out',
-    opacity: 0,
-    padding: 20,
-    display: 'inline-block',
-    backgroundColor: '#8787d8'
-}
-const transitionStyles = {
-    entering: { opacity: 0 },
-    entered: { opacity: 1 },
-}
 export default class Banner extends Component{
     constructor(props){
         super(props)
         this.state={
-            bStatus: true
+            bStatus: false
         }
     }
     hideBanner(){
@@ -26,18 +14,14 @@ export default class Banner extends Component{
             bStatus: !this.state.bStatus
         })
     }
-    render(){
-        if(this.state.bStatus){
-            return (
-                <div className='top-banner'>
-                    <div className='container'>
-                        <FA name='times' onClick={this.hideBanner.bind(this)} />
-                        <img src={banner}/>
-                    </div>
-                </div>            
-            )
-        }else{
-            return null
-        }
+  render(){
+        return (
+            <div className={['top-banner',this.state.bStatus&&'hide'].join(' ')}>
+                <div className='container'>
+                    <FA name='times' onClick={this.hideBanner.bind(this)} />
+                    <img src={banner}/>
+                </div>
+            </div>
+        )
     }
 }
