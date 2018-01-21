e92fc6d95d89c56fd2a4d51bf53d4234fbd9d905
rk <471091127@qq.com>
2018-01-21 19:48:11 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDom from 'react-dom'
 import Banner from './components/banner/index'
 import InfoNav from './components/info/index'
@@ -11,20 +11,76 @@ import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import  'font-awesome/css/font-awesome.css'
 import './assets/css/reset.css'
 import './assets/css/main.scss'
+import Music from './pages/music'
+import Movie from './pages/movie'
+import Handle from './pages/handle'
+import Others from './pages/others'
+const Home = () => {
+  return(
+    <div className='home'>
+      <Banner />
+      <InfoNav />
+      <div className='container'>
+        <Search />
+        <div className='clearfix'>
+          <MainMenu />
+          <SlideShow />
+          <SideService />
+        </div>
+        <MidddleShow />
+      </div>
+    </div>
+  )
+}
+class ReactApp extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+
+    }
+  }
+  render(){
+    return (
+        <Routers />
+    )
+  }
+}
+class Routers extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      defaultActive:1
+    }
+    this.changeRouter=this.changeRouter.bind(this)
+  }
+  changeRouter(event,index){
+    this.setState({
+      defaultActive:index
+    })
+  }
+render(){
+    return (
+      <Router>
+        <div className='out-container'>
+          <ul className='out-routers clearfix'>
+            <li className={this.state.defaultActive === 1 ?'router-active':''} onClick={(event)=>{this.changeRouter(event,1)}}><Link to="/">首页</Link></li>
+            <li className={this.state.defaultActive === 2 ? 'router-active' : ''} onClick={(event) => { this.changeRouter(event, 2) }}><Link to="/music">音乐</Link></li>
+            <li className={this.state.defaultActive === 3 ? 'router-active' : ''} onClick={(event) => { this.changeRouter(event, 3) }}><Link to="/movie">电影</Link></li>
+            <li className={this.state.defaultActive === 4 ? 'router-active' : ''} onClick={(event) => { this.changeRouter(event, 4) }}><Link to="/handle">操作</Link></li>
+            <li className={this.state.defaultActive === 5 ? 'router-active' : ''} onClick={(event) => { this.changeRouter(event, 5) }}><Link to="/others">其他</Link></li>
+          </ul>
+          <Route exact path="/" component={Home} />
+          <Route path="/music" component={Music} />
+          <Route path="/movie" component={Movie} />
+          <Route path="/handle" component={Handle} />
+          <Route path="/others" component={Others} />
+        </div>
+      </Router>
+    )
+  }
+}
 ReactDom.render(
 (
-  <div>
-    <Banner />
-    <InfoNav />
-    <div className='container'>
-      <Search />
-      <div className='clearfix'>
-       <MainMenu/>
-       <SlideShow />
-       <SideService />
-      </div>
-      <MidddleShow />
-    </div>
-  </div>
+    <ReactApp />
 ),
 document.getElementById('app'))
