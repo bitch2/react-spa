f977042ed522961d58dd4319349a2921a8a630a8
rk <471091127@qq.com>
2018-02-01 17:15:55 +0800

    server

--- server/index.js
+++ server/index.js
@@ -2,28 +2,24 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const formidable = require('formidable')
 const http=require('superagent')
+const path=require('path')
 const fs = require('fs')
 const app = express()
 const port = 8080
 app.use(bodyParser.urlencoded({extended:false}))
+app.use('/img',express.static(__dirname+'/img'))
 app.get('/',(req,res)=>{
     res.send('server listen on localhost:8080')
 })
 app.get('/upload/getInfo',(req,res,next)=>{
     res.send({'name':'admin','age':20})
 })
-app.post('/upload/saveImg',(req,res,next)=>{
-    // var form=new formidable.IncomingForm()
-    // form.encoding='utf-8'
-    // res.set({'Content-type':'application/json'})
-    var data=req.body
-    console.log(data)
+app.post('/upload/saveOnlineImg',(req,res,next)=>{
+    // 图片地址类
+    let data=req.body
     if(!fs.existsSync(__dirname+ '/img')){
         fs.mkdirSync(__dirname+'/img')
     }
-    // form.parse(req,(err,fields,files)=>{
-    //     console.log(files.the_file)
-    // })
     http.get(req.body.img)
     .set({'async':false})
     .end((err,msg)=>{
@@ -31,10 +27,38 @@ app.post('/upload/saveImg',(req,res,next)=>{
             return next(err)
         }
         msg.setEncoding('binary')
-        fs.writeFile(__dirname+'/img/'+req.body.name+'.png',rec.body,'binary',()=>{
+        fs.writeFile(__dirname+'/img/'+data.name+'.png',msg.body,'binary',(err)=>{
+            if(err){
+                res.send({'upload':false,'msg':'上传失败！'})
+            }
+            res.send({'upload':true,'msg':'上传成功!','url':'http://localhost:8080/img/'+data.name+'.png'})
         })
     })
-    res.send({'upload':true,'url':'http://localhost:8008/img'+req.body.name+'.png'})
+})
+app.post('/upload/saveLocalImg',(req,res,next)=>{
+    // 上传图片类
+    if(!fs.existsSync(__dirname+ '/img')){
+        fs.mkdirSync(__dirname+'/img')
+    }
+    let form=new formidable.IncomingForm()
+    form.encoding='utf-8'
+    form.keepExtensions=true
+    form.uploadDir = __dirname+'/img'
+    form.parse(req,(err,fields,files)=>{
+        if(err){
+            res.send({'upload':false,'msg':'上传失败!'})
+            return next(err)
+        }
+        let imgPath=files.img.path
+        let imgName=fields.name
+        let imgData=fs.readFileSync(imgPath)
+        fs.writeFile(imgPath,imgData,(err)=>{
+            if(err){
+                res.send({'upload':false,'msg':'上传失败!'})
+            }
+            res.send({'upload':true,'msg':'上传成功!','url':'http://localhost:8080/img/'+ path.basename(imgPath)})
+        })
+    })
 })
 app.listen(port,(err)=>{
     if(err){
