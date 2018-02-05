4a5332ae51014528a21760a3d55ef761425b7836
rk <471091127@qq.com>
2018-02-05 16:20:12 +0800

    主函数

--- src/main.js
+++ src/main.js
@@ -12,15 +12,16 @@ import  'font-awesome/css/font-awesome.css'
 import './assets/css/reset.css'
 import './assets/css/main.scss'
 import './assets/css/custom.css'
+import Home from './pages/home'
 import Music from './pages/music'
 import Movie from './pages/movie'
 import Handle from './pages/handle'
-import Others from './pages/others'
 import { Provider } from 'react-redux'
 import store from './store'
-const Home = () => {
+const Demo = () => {
   return(
-    <div className='home'>
+    <div className='demo'>
+      <p style={{paddingLeft:'20px',lineHeight:'40px',fontSize:'14px',fontWeight:'bold',color:'#0080f6',background:'#fff'}}>react渲染静态页面实验</p>
       <Banner />
       <InfoNav />
       <div className='container'>
@@ -35,54 +36,60 @@ const Home = () => {
     </div>
   )
 }
-class ReactApp extends Component{
-  constructor(props){
-    super(props)
-    this.state={
-
-    }
-  }
-  render(){
-    return (
-        <Routers />
-    )
-  }
-}
 class Routers extends Component{
   constructor(props){
     super(props)
     this.state={
-      defaultActive:1
+      defaultActive:1,
+      hash:''
     }
     this.changeRouter=this.changeRouter.bind(this)
   }
-  changeRouter(event,index){
+  componentWillMount(){
+    let hash=window.location.hash
     this.setState({
-      defaultActive:index
+      hash:hash
     })
   }
-render(){
+  changeRouter(){
+    let hash=window.location.hash
+    if(hash==='#/'){
+      this.setState({
+        hash:'#/'
+      })
+    }else{
+      this.setState({
+        hash:hash
+      })
+    }
+  }
+  render(){
     return (
       <Provider store={store}>
         <Router>
           <div className='out-container'>
             <ul className='out-routers clearfix'>
-              <li className={this.state.defaultActive === 1 ?'router-active':''} onClick={(event)=>{this.changeRouter(event,1)}}><Link to="/">首页</Link></li>
-              <li className={this.state.defaultActive === 2 ? 'router-active' : ''} onClick={(event) => { this.changeRouter(event, 2) }}><Link to="/music">音乐</Link></li>
-              <li className={this.state.defaultActive === 3 ? 'router-active' : ''} onClick={(event) => { this.changeRouter(event, 3) }}><Link to="/movie">电影</Link></li>
-              <li className={this.state.defaultActive === 4 ? 'router-active' : ''} onClick={(event) => { this.changeRouter(event, 4) }}><Link to="/handle">操作</Link></li>
-              <li className={this.state.defaultActive === 5 ? 'router-active' : ''} onClick={(event) => { this.changeRouter(event, 5) }}><Link to="/others">其他</Link></li>
+              <li className={this.state.hash==='#/'?'router-active' : ''} onClick={this.changeRouter}><Link to="/">首页</Link></li>
+              <li className={this.state.hash.indexOf('music') !== -1 ? 'router-active' : ''} onClick={this.changeRouter}><Link to="/music">音乐</Link></li>
+              <li className={this.state.hash.indexOf('movie') !== -1 ? 'router-active' : ''} onClick={this.changeRouter}><Link to="/movie">电影</Link></li>
+              <li className={this.state.hash.indexOf('handle') !== -1 ? 'router-active' : ''} onClick={this.changeRouter}><Link to="/handle">操作</Link></li>
+              <li className={this.state.hash.indexOf('demo') !== -1 ? 'router-active' : ''} onClick={this.changeRouter}><Link to="/demo">例子</Link></li>
             </ul>
             <Route exact path="/" component={Home} />
             <Route path="/music" component={Music} />
             <Route path="/movie" component={Movie} />
             <Route path="/handle" component={Handle} />
-            <Route path="/others" component={Others} />
+            <Route path="/demo" component={Demo} />
           </div>
         </Router>
       </Provider>
     )
   }
+}
+const ReactApp = () => {
+  return (
+    <Routers />
+  )
 }
 ReactDom.render(
 (
