df0a41bfdc9f79dc5a349861702ea00ebaa05702
rk <471091127@qq.com>
2018-02-05 16:57:45 +0800

    c操作图片预览

--- src/pages/handle/index.js
+++ src/pages/handle/index.js
@@ -3,19 +3,24 @@ import 'whatwg-fetch'
 import './index.scss'
 import avatar from '../../assets/img/user_icon.jpg'
 import custom from '../../assets/js/custom'
+import FA from 'react-fontawesome'
 export default class Handle extends Component {
     constructor(props){
       super(props)
       this.state={
         onlineImg:'https://avatars0.githubusercontent.com/u/17215880?s=460&v=4',
         onlineAvatarImg:'',
-        localAvatarImg:''
+        localAvatarImg:'',
+        previewImg:'',
+        modalState:false
       }
       this.handleWaiting=this.handleWaiting.bind(this)
       this.handleLoading=this.handleLoading.bind(this)
       this.msgRemind=this.msgRemind.bind(this)
       this.localUpload=this.localUpload.bind(this)
       this.confirmUpload=this.confirmUpload.bind(this)
+      this.previewImg=this.previewImg.bind(this)
+      this.closePreview=this.closePreview.bind(this)
     }
     msgRemind(){
       custom.messages.normal()
@@ -25,17 +30,17 @@ export default class Handle extends Component {
     }
     handleWaiting(){
       custom.waiting.show()
-      custom.messages.warning('三秒后关闭')
+      custom.messages.warning('2s 后关闭')
       setTimeout(()=>{
         custom.waiting.hide()
-      },3000)    
+      },2000)    
     }
     handleLoading(){
       custom.loading.show()
-      custom.messages.warning('三秒后关闭')
+      custom.messages.warning('2s 后关闭')
       setTimeout(()=>{
         custom.loading.hide()
-      },3000)
+      },2000)
     }
     confirmUpload(){
       let data=new URLSearchParams()
@@ -62,7 +67,7 @@ export default class Handle extends Component {
       })
       .catch((err)=>{
         custom.loading.hide()
-        custom.messages.error('服务出错!')
+        custom.messages.error('服务出错！')
         console.log(err)
       })
     }
@@ -91,8 +96,23 @@ export default class Handle extends Component {
       })
       .catch((err)=>{
         custom.loading.hide()
-        custom.messages.error('服务出错!')
+        custom.messages.error('服务出错！')
         console.log(err)
+      })
+    }
+    previewImg(img){
+      if(img===''){
+        custom.messages.warning('请先上传图片！')
+        return
+      }
+      this.setState({
+        modalState:true,
+        previewImg:img
+      })
+    }
+    closePreview(){
+      this.setState({
+        modalState:false
       })
     }
     render() {
@@ -112,7 +132,7 @@ export default class Handle extends Component {
             <p>图片地址：{this.state.onlineImg}</p>
             <button className='btn btn-primary' onClick={this.confirmUpload}>确认上传</button>
             <div className='img-preview'>
-              <img className='avatarImg' src={this.state.onlineAvatarImg===''?avatar:this.state.onlineAvatarImg} alt='预览' />
+              <img className='avatarImg' onClick={(e)=>{this.previewImg(this.state.onlineAvatarImg)}} src={this.state.onlineAvatarImg===''?avatar:this.state.onlineAvatarImg} alt='预览' />
             </div>
           </div>
           <div className='handle-item'>
@@ -122,8 +142,12 @@ export default class Handle extends Component {
               <input className='upload-btn' type='file' ref='file' accept='image/jpeg,image/x-png,image/gif' onChange={(e)=>{this.localUpload(e.target.files[0])}}/>
             </div>
             <div className='img-preview'>
-              <img className='avatarImg' src={this.state.localAvatarImg===''?avatar:this.state.localAvatarImg} alt='预览' />
+              <img className='avatarImg' onClick={(e)=>{this.previewImg(this.state.localAvatarImg)}} src={this.state.localAvatarImg===''?avatar:this.state.localAvatarImg} alt='预览' />
             </div>
+          </div>
+          <div className={this.state.modalState?'modal-preview show':'modal-preview'}>
+            <img src={this.state.previewImg} />
+            <FA name='times-circle' onClick={this.closePreview} />
           </div>
         </div>
       )
